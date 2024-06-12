91286ddd6081a5731160d179018198114b87edc4
doctorpangloss <@hiddenswitch.com>
2024-06-11 17:11:45 -0700

    Scaffolding

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # appmana-comfyui-after-effects-plugin
+
 Use ComfyUI from After Effects
+
+## Development
+
+Download the After Effects and Premiere 2022 SDKs from https://developer.adobe.com/console/servicesandapis/ and
+extract `AfterEffectsSDK` and `Premiere Pro 22.0 C++ SDK` directories to located in `./sdk`.
+
+**Windows Prerequisites**
+
+```shell
+# must be separate
+choco install -y visualstudio2022buildtools
+# includes clang + msvc
+choco install -y visualstudio2022-workload-vctools --package-parameters "--add Microsoft.VisualStudio.Component.VC.Llvm.ClangToolset --add Microsoft.VisualStudio.Component.VC.Llvm.Clang"
+choco install -y rustup.install
+# verify
+rustc --version
+cargo --version
+# include standard library
+rustup component add rust-src
+# rust build tool "just"
+cargo install just
+```
+
+**Build**
+
+```powershell
+export LIBCLANG_PATH="C:/Program Files (x86)/Microsoft Visual Studio/2022/BuildTools/VC/Tools/Llvm/x64/bin"
+just build
+```
+
+### Contributions
+
+Initialized from https://github.com/AdrianEddy/after-effects/blob/master/examples/portable/build.rs
---
+++ src/lib.rs
@@ -0,0 +1,159 @@
+use after_effects as ae;
+
+#[derive(Eq, PartialEq, Hash, Clone, Copy, Debug)]
+enum Params {
+  MixChannels,
+}
+
+#[derive(Default)]
+struct Plugin {}
+
+ae::define_effect!(Plugin, (), Params);
+
+fn detect_host(in_data: ae::InData) -> String {
+  use ae::sys::*;
+  let v = (in_data.version().0 as u32, in_data.version().1 as u32);
+
+  let app = match &in_data.application_id() {
+    #[rustfmt::skip]
+    b"FXTC" => {
+      if v.0 >= 12 {
+        if v.0 == PF_AE234_PLUG_IN_VERSION && v.1 >= PF_AE234_PLUG_IN_SUBVERS { "After Effects 2023 (23.4) or later." }
+        else if v.0 == PF_AE220_PLUG_IN_VERSION && v.1 == PF_AE220_PLUG_IN_SUBVERS { "After Effects 2022 (22.0)." }
+        else if v.0 == PF_AE184_PLUG_IN_VERSION && v.1 == PF_AE184_PLUG_IN_SUBVERS { "After Effects 2021 (18.4)." }
+        else if v.0 == PF_AE182_PLUG_IN_VERSION && v.1 == PF_AE182_PLUG_IN_SUBVERS { "After Effects 2021 (18.2)." }
+        else if v.0 == PF_AE180_PLUG_IN_VERSION && v.1 == PF_AE180_PLUG_IN_SUBVERS { "After Effects 2021 (18.0)." }
+        else if v.0 == PF_AE177_PLUG_IN_VERSION && v.1 == PF_AE177_PLUG_IN_SUBVERS { "After Effects 2020 (17.7)." }
+        else if v.0 == PF_AE176_PLUG_IN_VERSION && v.1 == PF_AE176_PLUG_IN_SUBVERS { "After Effects 2020 (17.6)." }
+        else if v.0 == PF_AE175_PLUG_IN_VERSION && v.1 == PF_AE175_PLUG_IN_SUBVERS { "After Effects 2020 (17.5)." }
+        else if v.0 == PF_AE171_PLUG_IN_VERSION && v.1 >= PF_AE171_PLUG_IN_SUBVERS { "After Effects 2020 (17.1)." }
+        else if v.0 == PF_AE170_PLUG_IN_VERSION && v.1 == PF_AE170_PLUG_IN_SUBVERS { "After Effects 2020 (17.0)." }
+        else if v.0 == PF_AE161_PLUG_IN_VERSION && v.1 == PF_AE161_PLUG_IN_SUBVERS { "After Effects 2019 (16.1)." }
+        else if v.0 == PF_AE160_PLUG_IN_VERSION && v.1 >= PF_AE160_PLUG_IN_SUBVERS { "After Effects CC 2019 (16.0)." }
+        else if v.0 == PF_AE151_PLUG_IN_VERSION && v.1 >= PF_AE151_PLUG_IN_SUBVERS { "After Effects CC 2018 (15.1)." }
+        else if v.0 == PF_AE150_PLUG_IN_VERSION && v.1 >= PF_AE150_PLUG_IN_SUBVERS { "After Effects CC 2017 (15.0)." }
+        else if v.0 == PF_AE140_PLUG_IN_VERSION && v.1 >= PF_AE140_PLUG_IN_SUBVERS { "After Effects CC 2017 (14.0)." }
+        else if v.0 == PF_AE138_PLUG_IN_VERSION && v.1 == PF_AE138_PLUG_IN_SUBVERS { "After Effects CC 2015.3 (13.8)." }
+        else if v.0 == PF_AE136_PLUG_IN_VERSION && v.1 == PF_AE136_PLUG_IN_SUBVERS { "After Effects CC 2015.1 or 2015.2 (13.6 or 13.7)."  }
+        else if v.0 == PF_AE135_PLUG_IN_VERSION && v.1 == PF_AE135_PLUG_IN_SUBVERS { "After Effects CC 2015 (13.5)." }
+        else if v.0 == PF_AE130_PLUG_IN_VERSION && v.1 == PF_AE130_PLUG_IN_SUBVERS { "After Effects CC 2014 (13.0 - 13.2)." }
+        else if v.0 == PF_AE122_PLUG_IN_VERSION && v.1 == PF_AE122_PLUG_IN_SUBVERS { "After Effects CC (12.2)." }
+        else if v.0 == PF_AE121_PLUG_IN_VERSION && v.1 == PF_AE121_PLUG_IN_SUBVERS { "After Effects CC (12.1)." }
+        else if v.0 == PF_AE120_PLUG_IN_VERSION && v.1 == PF_AE120_PLUG_IN_SUBVERS { "After Effects CC (12.0)." }
+        else if v.0 == PF_AE1101_PLUG_IN_VERSION && v.1 == PF_AE1101_PLUG_IN_SUBVERS { "After Effects CS6.0.1 or CS6.0.2." }
+        else if v.0 == PF_AE110_PLUG_IN_VERSION && v.1 == PF_AE110_PLUG_IN_SUBVERS { "After Effects CS6.0." }
+        else {
+          // Q. How can I tell the difference between versions where the API version is the same, such as AE 6.5 and 7.0?
+          // A. The effect API didn't change the only way to differentiate between them is to check for the presence of a version of a suite new in 7.0.
+          // Say, something 32bpc-ish. To avoid AEGP_SuiteHandler throwing if the suite isn't present, we'll acquire it the old-school way.
+          if pf::suites::IterateFloat::new().is_ok() {
+            "After Effects between 7.0 and CS4."
+          } else {
+            "After Effects 6.5 or earlier."
+          }
+        }
+      } else { // Wow, an antique!
+        "some unknown version of After Effects!"
+      }
+    }
+    b"PrMr" => {
+      // let pixel_format = ae::pf::PixelFormatSuite::new().unwrap();
+      // pixel_format.clear_supported_pixel_formats(in_data.effect_ref()).unwrap();
+      // pixel_format.add_supported_pixel_format(in_data.effect_ref(), ae_sys::PrPixelFormat_PrPixelFormat_VUYA_4444_32f).unwrap();
+
+      // The major/minor versions provide basic differentiation.
+      // If you need finer granularity, e.g. differentiating between
+      // PPro CC 2015.3 and CC 2017, then use the App Info Suite from/ the Premiere Pro SDK
+      if v.0 == 13 && v.1 >= 4 {
+        "Premiere Pro CC, CC 2014, or later!"
+      } else if v.0 == 13 && v.1 == 2 {
+        "Premiere Pro CS6!"
+      } else {
+        "some unknown version of Premiere!"
+      }
+    }
+    _ => "some oddball host.",
+  };
+
+  log::info!("Running in {app}");
+
+  format!("Running in {app}")
+}
+
+impl AdobePluginGlobal for Plugin {
+  fn can_load(_host_name: &str, _host_version: &str) -> bool {
+    true
+  }
+
+  fn params_setup(&self, params: &mut ae::Parameters<Params>, _in_data: InData, _out_data: OutData) -> Result<(), Error> {
+    params.add(Params::MixChannels, "Mix channels", ae::FloatSliderDef::setup(|f| {
+      f.set_valid_min(0.0);
+      f.set_slider_min(0.0);
+      f.set_valid_max(200.0);
+      f.set_slider_max(200.0);
+      f.set_value(10.0);
+      f.set_default(10.0);
+      f.set_precision(1);
+      f.set_display_flags(ae::ValueDisplayFlag::PERCENT);
+    }))
+  }
+
+  fn handle_command(&mut self, cmd: ae::Command, in_data: ae::InData, mut out_data: ae::OutData, params: &mut ae::Parameters<Params>) -> Result<(), ae::Error> {
+    match cmd {
+      ae::Command::About => {
+        out_data.set_return_msg("Portable, v3.3\rThis example shows how to detect and respond to different hosts.\rCopyright 2007-2023 Adobe Inc.");
+      }
+      ae::Command::SequenceSetup => {
+        out_data.set_return_msg(&detect_host(in_data));
+      }
+      ae::Command::Render { in_layer, mut out_layer } => {
+        let slider_value = params.get(Params::MixChannels)?.as_float_slider()?.value() as f32;
+
+        // If the slider is 0 just make a direct copy.
+        if slider_value < 0.001 {
+          out_layer.copy_from(&in_layer, None, None)?;
+        } else {
+          let extent_hint = in_data.extent_hint();
+          let out_extent_hint = out_layer.extent_hint();
+          // clear all pixels outside extent_hint.
+          if extent_hint != out_extent_hint {
+            out_layer.fill(None, Some(out_extent_hint))?;
+          }
+
+          // iterate over image data.
+          #[rustfmt::skip]
+          in_layer.iterate_with(&mut out_layer, 0, extent_hint.height(), Some(extent_hint), |_x: i32, _y: i32, pixel: ae::GenericPixel, out_pixel: ae::GenericPixelMut| -> Result<(), Error> {
+            let pixel = pixel.as_f32();
+
+            // Mix the values. The higher the slider, the more we blend the channel with the average of all channels
+            let average = (pixel.red + pixel.green + pixel.blue) / 3.0;
+            // let midway_calc = (slider_value * average) + (200.0 - slider_value) * pixel.red;
+
+            let r = ((slider_value * average) + (100.0 - slider_value) * pixel.red  ) / 100.0;
+            let g = ((slider_value * average) + (100.0 - slider_value) * pixel.green) / 100.0;
+            let b = ((slider_value * average) + (100.0 - slider_value) * pixel.blue ) / 100.0;
+
+            match out_pixel {
+              ae::GenericPixelMut::Pixel8(out_pixel) => {
+                out_pixel.alpha = pixel.alpha as _;
+                out_pixel.red   = r.min(ae::MAX_CHANNEL8 as f32) as _;
+                out_pixel.green = g.min(ae::MAX_CHANNEL8 as f32) as _;
+                out_pixel.blue  = b.min(ae::MAX_CHANNEL8 as f32) as _;
+              }
+              ae::GenericPixelMut::Pixel16(out_pixel) => {
+                out_pixel.alpha = pixel.alpha as _;
+                out_pixel.red   = r.min(ae::MAX_CHANNEL16 as f32) as _;
+                out_pixel.green = g.min(ae::MAX_CHANNEL16 as f32) as _;
+                out_pixel.blue  = b.min(ae::MAX_CHANNEL16 as f32) as _;
+              }
+              _ => return Err(Error::BadCallbackParameter)
+            }
+            Ok(())
+          })?;
+        }
+      }
+      _ => {}
+    }
+    Ok(())
+  }
+}
